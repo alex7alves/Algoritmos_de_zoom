4d8651dfd2307444a925112457fe2aaf5035e603
Alex Alves <alex7alves10@hotmail.com>
2017-11-11 17:42:43 -0300

    Melhorando o programa

--- VizinhoMaisProximo.cpp
+++ VizinhoMaisProximo.cpp
@@ -1,68 +1,108 @@
-// Método do vizinho mais proximo
-// Autor: Alex Alves
 
+/*
+    Método do vizinho mais proximo
+    para zoom em imagem
+
+    Autor: Alex Alves
+
+*/
 #include<iostream>
 #include<stdio.h>
 #include<stdlib.h>
 using namespace std;
 
-void VizinhoMaisProximo(int tamanho, int fator,int **imagem, int **zoom);
+void AlocarFator(int **tamanho, int **fator);
+void LerFator(int *tamanho, int *fator);
+void AlocarMatriz(int ***imagem,int ***zoom,int *tamanho, int *fator);
+void LerMatriz(int **imagem,int**zoom,int *tamanho, int *fator);
+void VizinhoMaisProximo(int *tamanho, int *fator,int **imagem, int **zoom);
+void MostrarMatriz(int**zoom,int *tamanho, int *fator);
 
 int main()
 {
-
-    int tamanho =3,fator =4;
+    int *tamanho,*fator;
     //Criando matriz dinamica
     int **imagem, **zoom;
-    imagem = (int **) malloc(tamanho*sizeof(int*));
-    zoom = (int **) malloc((fator*tamanho)*sizeof(int*));
-    for(int i=0;i<tamanho;i++){
-        for(int j=0;j<tamanho;j++){
-            imagem[i] = (int *)malloc(tamanho * sizeof(int));
-        }
-    }
-    for(int i=0;i<(fator*tamanho);i++){
-        for(int j=0;j<(fator*tamanho);j++){
-            zoom[i] = (int *)malloc((fator*tamanho) * sizeof(int));
-        }
-    }
-
+    AlocarFator(&tamanho,&fator);
+    LerFator(tamanho,fator);
+    AlocarMatriz(&imagem,&zoom,tamanho,fator);
+    LerMatriz(imagem,zoom,tamanho,fator);
     VizinhoMaisProximo(tamanho,fator,imagem,zoom);
-
+    MostrarMatriz(zoom,tamanho,fator);
     return 0;
 }
 
-void VizinhoMaisProximo(int tamanho, int fator,int **imagem, int **zoom){
-    for (int i =0;i<tamanho;i++){
-        for(int j=0;j<tamanho;j++){
-            cin >> imagem[i][j];
+void AlocarFator(int **tamanho, int **fator)
+{
+    *tamanho = (int *)malloc(sizeof(int));
+    *fator = (int *)malloc(sizeof(int));
+}
+void LerFator(int *tamanho, int *fator)
+{
+    printf("Digite a ordem da matriz\n");
+    //scanf("%d\n",tamanho);
+    cin >> *tamanho;
+    printf("Digite o fator de zoom\n");
+   // scanf("%d\n",fator);
+    cin >> *fator;
+}
+
+void AlocarMatriz(int ***imagem,int ***zoom,int *tamanho, int *fator)
+{
+
+    *imagem = (int **)malloc((*tamanho)*sizeof(int*));
+    *zoom = (int **) malloc((*fator)*(*tamanho)*sizeof(int*));
+    for(int i=0;i<*tamanho;i++){
+        (*imagem)[i] = (int *)malloc(*tamanho * sizeof(int));
+    }
+    for(int i=0;i<((*fator)*(*tamanho));i++){
+        (*zoom)[i] = (int *)malloc((*fator)*(*tamanho) * sizeof(int));
+    }
+}
+
+void LerMatriz(int **imagem,int**zoom,int *tamanho, int *fator)
+{
+    cout << " Digite a matriz" << endl;
+    for (int i =0;i<*tamanho;i++){
+        for(int j=0;j<*tamanho;j++){
+             cin >>imagem[i][j];
         }
     }
-    for (int i =0;i<(fator*tamanho);i++){
-        for(int j=0;j<(fator*tamanho);j++){
+}
+
+void VizinhoMaisProximo(int *tamanho, int *fator,int **imagem, int **zoom)
+{
+
+    for (int i =0;i<(*fator)*(*tamanho);i++){
+        for(int j=0;j<(*fator)*(*tamanho);j++){
              zoom[i][j]=0;
         }
     }
     int cont=0,linhaImagem=0;
-    for (int i =0;i<(fator*tamanho);i++){
-         if(cont==fator){
+    for (int i =0;i<(*fator)*(*tamanho);i++){
+         if(cont==*fator){
             linhaImagem = linhaImagem+1;
             cont=0;
         }
-        for(int j=0,ColunaImagem=0;j<(fator*tamanho),ColunaImagem<tamanho;j=j+fator,ColunaImagem++){
+        for(int j=0,ColunaImagem=0;j<(*fator)*(*tamanho),ColunaImagem<*tamanho;j=j + *fator,ColunaImagem++){
 
             zoom[i][j]=imagem[linhaImagem][ColunaImagem];
-            for(int k=1; k<fator;k++){
+            for(int k=1; k<*fator;k++){
                 zoom[i][j+k]=imagem[linhaImagem][ColunaImagem];
             }
         }
-        if(linhaImagem>=tamanho){
+        if(linhaImagem>=*tamanho){
             linhaImagem=0;
         }
         cont++;
     }
-    for (int i =0;i<(fator*tamanho);i++){
-        for(int j=0;j<(fator*tamanho);j++){
+
+}
+
+void MostrarMatriz(int**zoom,int *tamanho, int *fator)
+{
+    for (int i =0;i<(*fator)*(*tamanho);i++){
+        for(int j=0;j<(*fator)*(*tamanho);j++){
            cout <<  zoom[i][j]<< " ";
         }
         cout << endl;
